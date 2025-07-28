1a501238b7c3a350b60c0200c06f38aefd895129
shevious <shevious@yahoo.com>
2025-07-28 13:52:28 +0900

    week01 update

--- week01/02_final/ModOperator.java
+++
@@ -1,9 +0,0 @@
-// ModOperator.java
-public class ModOperator {
-    public static void main(String[] args) {
-         System.out.println(5%3); // 2
-         System.out.println(3%2.5); // 0.5
-         System.out.println(-22%3); // -1
-         System.out.println(22%-3); // 1
-    }
-}
