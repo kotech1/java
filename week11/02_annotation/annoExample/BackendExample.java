a5b1740de5650ab5bfdafd7a195c00f60b0f45cd
shevious <shevious@yahoo.com>
2025-08-27 11:03:20 +0900

    week11 annoExample update

--- week11/02_annotation/annoExample/BackendExample.java
+++ week11/02_annotation/annoExample/BackendExample.java
@@ -1,10 +1,9 @@
 package annoExample;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class BackendExample {
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) {
         String path = "/login";
 
         UserController uc = new UserController();
@@ -14,13 +13,18 @@ public class BackendExample {
         // UserController의 모든 메소드를 얻어옴.
         Method[] methods = uc.getClass().getDeclaredMethods();
 
-        for (Method mt : methods) {
+        for (Method m : methods) {
             // System.out.println(mt.getName()); 메소드 명칭
-            RequestMapping anno = mt.getAnnotation(RequestMapping.class);
+            // RequestMapping 애너테이션을 가져옴.
+            RequestMapping anno = m.getAnnotation(RequestMapping.class);
             if (anno == null) continue; // RequestMapping 애너테이션 없음.
 
             if (anno.uri().equals(path))
-                mt.invoke(uc);
+                try {
+                    m.invoke(uc);       // 해당 메소드 호출
+                } catch (Exception e) { // 구체적 명시 필요
+                    e.printStackTrace(); 
+                }
         }
     }
 }
