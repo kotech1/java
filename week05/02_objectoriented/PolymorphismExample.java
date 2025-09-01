4330835164dd1da74bf060a50d9d078ab9f85dad
shevious <shevious@yahoo.com>
2025-09-01 20:26:20 +0900

    week05 polymorphism update

--- week05/02_objectoriented/PolymorphismExample.java
+++ week05/02_objectoriented/PolymorphismExample.java
@@ -16,8 +16,8 @@ class Cat2 implements Animal2 {
 
 public class PolymorphismExample {
     public static void main(String[] args) { 
-        Animal2 a1 = new Dog2();
-        Animal2 a2 = new Cat2();
+        Animal2 a1 = new Dog2(); // 업 캐스팅
+        Animal2 a2 = new Cat2(); // 업 캐스팅
 
         a1.sound();  // 출력: 멍멍!
         a2.sound();  // 출력: 야옹~
