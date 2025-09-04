91fe248460d2f34593495224ac9aa15c05f5f0b6
shevious <shevious@yahoo.com>
2025-09-04 18:52:29 +0900

    week12

---
+++ week12/01_list/ListExample2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class User {
+    long id;
+    String name;
+    String email; //optional
+    User(long id, String name) {
+        this.id = id; this.name = name;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof User u)) return false;
+        
+        return this.id == u.id && this.name.equals(u.name);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
+public class ListExample2 {
+    public static void main(String[] args) {
+        List<User> l = new ArrayList<>(); // JDK7+
+        l.add(new User(1001, "홍길동"));
+        l.add(new User(1002, "김영희"));
+
+        // true or false?
+        System.out.println("홍길동 포함? "
+            + l.contains(new User(1001, "홍길동"))); 
+        l.remove(new User(1001, "홍길동"));
+        l.forEach((u) -> System.out.println(u.name));
+    }
+}
